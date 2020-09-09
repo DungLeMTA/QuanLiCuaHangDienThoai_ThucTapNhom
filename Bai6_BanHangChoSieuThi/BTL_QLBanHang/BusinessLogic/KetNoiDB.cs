78c54528f79ca4b4afa02a9eb7b2b788abfca019
DungLeMTA <duydung.glhd99@gmail.com>
2020-09-09 08:13:14 +0700

    Dũng

---
+++ Bai6_BanHangChoSieuThi/BTL_QLBanHang/BusinessLogic/KetNoiDB.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace BusinessLogic
+{
+    public class KetNoiDB
+    {
+        SqlConnection cn = new SqlConnection();
+        static public String getconnect()
+        {
+            return (@"Data Source=DESKTOP-SAU51IQ;Initial Catalog=Bai6_BanHangSieuThi;Integrated Security=True");
+
+        }
+    }
+
+}
