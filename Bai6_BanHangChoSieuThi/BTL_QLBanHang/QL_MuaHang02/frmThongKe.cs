b4998c1bcff45a44d79560b9832c42584c931d50
trananh99 <54661281+trananh99@users.noreply.github.com>
2020-09-14 20:10:46 +0700

    Van Anh

---
+++ Bai6_BanHangChoSieuThi/BTL_QLBanHang/QL_MuaHang02/frmThongKe.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using BusinessLogic;
+
+namespace QuanLyBanHang
+{
+    public partial class frmThongKe : Form
+    {
+        public frmThongKe()
+        {
+            InitializeComponent();
+        }
+
+        SanPham sp = new SanPham();
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (comboBox1.SelectedIndex == 6) dateTimePicker1.Enabled = dateTimePicker2.Enabled = true;
+            else dateTimePicker1.Enabled = dateTimePicker2.Enabled = false;
+        }
+
+        private void btnTK_Click(object sender, EventArgs e)
+        {
+           
+        }
+    }
+}
