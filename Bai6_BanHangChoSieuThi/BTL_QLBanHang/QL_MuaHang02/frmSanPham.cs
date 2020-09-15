2031177ef43b91ae37ea4b8b0a0b7af1333a4473
cuongmta99 <67366161+cuongmta99@users.noreply.github.com>
2020-09-15 23:58:27 +0700

    Update frmSanPham.cs

--- Bai6_BanHangChoSieuThi/BTL_QLBanHang/QL_MuaHang02/frmSanPham.cs
+++ Bai6_BanHangChoSieuThi/BTL_QLBanHang/QL_MuaHang02/frmSanPham.cs
@@ -45,13 +45,12 @@ namespace QuanLyBanHang
             }
             Init();
         }
-        public void HienThi2()
+        public void KhoiTaotxtsSP()
         {
-            dgvKhachHang.DataSource = kh.HienThiKhachHang();
-            for (int i = 0; i < dgvKhachHang.RowCount; i++)
-                dgvKhachHang.Rows[i].Cells[0].Value = (i + 1).ToString();
-
-            dgvSanPham.DataSource = sp.HienThiSP_LH();
+            txtTenSP.Text = txtTenNSX.Text = cmbGiaBan.Text = cmbTenLH.Text = "";
+            txtSoLuongMua.Text = "";
+            txtMaSP.Text = txtSoLgHangCon.Text = txtGiaBan.Text = "";
+            dongSP = 0;
         }
         public void KhoiTaotxtsKH()
         {
