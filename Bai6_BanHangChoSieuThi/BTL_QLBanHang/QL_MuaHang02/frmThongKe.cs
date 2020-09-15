df731e731786c8355c973f9ae83ef7c0f367bb7d
trananh99 <54661281+trananh99@users.noreply.github.com>
2020-09-15 20:11:18 +0700

    Van Anh

--- Bai6_BanHangChoSieuThi/BTL_QLBanHang/QL_MuaHang02/frmThongKe.cs
+++ Bai6_BanHangChoSieuThi/BTL_QLBanHang/QL_MuaHang02/frmThongKe.cs
@@ -27,7 +27,35 @@ namespace QuanLyBanHang
 
         private void btnTK_Click(object sender, EventArgs e)
         {
-           
+            if (comboBox2.SelectedIndex == 0)
+            {
+                switch (comboBox1.SelectedIndex)
+                {
+                    case -1: return;
+                    case 0: dataGridView1.DataSource = sp.ThongKeSPN(DateTime.Now.AddDays(-1), DateTime.Now); break;
+                    case 1: dataGridView1.DataSource = sp.ThongKeSPN(DateTime.Now.AddDays(-7), DateTime.Now); break;
+                    case 2: dataGridView1.DataSource = sp.ThongKeSPN(DateTime.Now.AddMonths(-1), DateTime.Now); break;
+                    case 3: dataGridView1.DataSource = sp.ThongKeSPN(DateTime.Now.AddMonths(-3), DateTime.Now); break;
+                    case 4: dataGridView1.DataSource = sp.ThongKeSPN(DateTime.Now.AddMonths(-6), DateTime.Now); break;
+                    case 5: dataGridView1.DataSource = sp.ThongKeSPN(DateTime.Now.AddYears(-1), DateTime.Now); break;
+                    case 6: dateTimePicker1.Enabled = dateTimePicker2.Enabled = true; dataGridView1.DataSource = sp.ThongKeSPN(dateTimePicker1.Value, dateTimePicker2.Value); break;
+                }
+            }
+            else if (comboBox2.SelectedIndex == 1)
+            {
+                switch (comboBox1.SelectedIndex)
+                {
+                    case -1: return;
+                    case 0: dataGridView1.DataSource = sp.ThongKeTop10(DateTime.Now.AddDays(-1), DateTime.Now); break;
+                    case 1: dataGridView1.DataSource = sp.ThongKeTop10(DateTime.Now.AddDays(-7), DateTime.Now); break;
+                    case 2: dataGridView1.DataSource = sp.ThongKeTop10(DateTime.Now.AddMonths(-1), DateTime.Now); break;
+                    case 3: dataGridView1.DataSource = sp.ThongKeTop10(DateTime.Now.AddMonths(-3), DateTime.Now); break;
+                    case 4: dataGridView1.DataSource = sp.ThongKeTop10(DateTime.Now.AddMonths(-6), DateTime.Now); break;
+                    case 5: dataGridView1.DataSource = sp.ThongKeTop10(DateTime.Now.AddYears(-1), DateTime.Now); break;
+                    case 6: dateTimePicker1.Enabled = dateTimePicker2.Enabled = true; dataGridView1.DataSource = sp.ThongKeSPN(dateTimePicker1.Value, dateTimePicker2.Value); break;
+                }
+            }
+            for (int i = 0; i < dataGridView1.RowCount - 1; i++) dataGridView1.Rows[i].Cells[0].Value = (i + 1).ToString();
         }
     }
 }
