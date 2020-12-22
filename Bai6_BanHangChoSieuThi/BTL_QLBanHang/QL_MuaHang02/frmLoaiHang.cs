a3dfe9826532f0f4326e056fd462dc89b329c43c
trananh99 <54661281+trananh99@users.noreply.github.com>
2020-12-22 15:04:13 +0700

    Add files via upload

---
+++ Bai6_BanHangChoSieuThi/BTL_QLBanHang/QL_MuaHang02/frmLoaiHang.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using BusinessLogic;
+
+namespace QuanLyBanHang
+{
+    public partial class frmLoaiHang : Form
+    {
+        public frmLoaiHang()
+        {
+            InitializeComponent();
+        }
+
+        LoaiHang lh = new LoaiHang();
+        private void frmLoaiHang_Load(object sender, EventArgs e)
+        {
+            HienThi();
+        }
+        public void HienThi()
+        {
+            dataGridView1.DataSource = lh.ShowLoaiHang();
+            for (int i = 0; i < dataGridView1.RowCount - 1; i++)
+            {
+                dataGridView1.Rows[i].Cells[0].Value = (i + 1).ToString();
+            }
+            Init();
+        }
+        public void Init()
+        {
+            if (dataGridView1.RowCount > 1)
+            {
+                try
+                {
+                    txtMaLH.Text = dataGridView1.Rows[0].Cells[1].Value.ToString();
+                    txtTenLH.Text = dataGridView1.Rows[0].Cells[2].Value.ToString();
+                }
+                catch { }
+            }
+        }
+        public void SetNull()
+        {
+            txtMaLH.Text = "";
+            txtTenLH.Text = "";
+        }
+        private void btnThem_Click(object sender, EventArgs e)
+        {
+            SetNull();
+            btnThem.Enabled = false;
+            btnLuu.Enabled = true;
+            btnHuy.Enabled = true;
+        }
+
+        private void btnSua_Click(object sender, EventArgs e)
+        {
+            lh.UpdateLoaiHang(txtMaLH.Text, txtTenLH.Text);
+            MessageBox.Show("Sửa dữ liệu thành công !!!", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            HienThi();
+        }
+
+       
+        private void btnThoat_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            try
+            {
+                txtMaLH.Text = dataGridView1.Rows[e.RowIndex].Cells[1].Value.ToString();
+                txtTenLH.Text = dataGridView1.Rows[e.RowIndex].Cells[2].Value.ToString();               
+            }
+            catch { }
+        }
+    }
+}
